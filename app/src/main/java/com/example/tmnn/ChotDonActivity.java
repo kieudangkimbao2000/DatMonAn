ec5f90839538fac416a451af90a8a7d6bd55c3c6
Kiều Đặng Kim Bảo <kieudangkimbao2000@outlook.com>
2021-03-28 17:49:18 +0700

    Tam thoi hoan chinh

---
+++ app/src/main/java/com/example/tmnn/ChotDonActivity.java
@@ -0,0 +1,14 @@
+package com.example.tmnn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChotDonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chot_don2);
+    }
+}
---
+++ app/src/main/java/com/example/tmnn/Item.java
@@ -0,0 +1,4 @@
+package com.example.tmnn;
+
+public class Item {
+}
